80c7b2a864d4f485972804f310cffe3529008c8a
aaronbolado <aaronjohncbolado@iskolarngbayan.pup.edu.ph>
2024-04-12 23:33:09 +0800

    Try

--- server/config/db.js
+++ server/config/db.js
@@ -1,1 +1,13 @@
+const mongoose = require('mongoose');
 // db here
+
+const connectDb = async () => {
+    try {
+        await mongoose.connect('mongodb+srv://aaronjohnbolado:QQlNejAU6NlPXA3b@cluster0.wfeebhy.mongodb.net/');
+        console.log('Connection to Database: Success');
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+module.exports = connectDb;
--- server/server.js
+++ server/server.js
@@ -1,1 +1,10 @@
-console.log("hello world");
+const express = require('express');
+const app = express();
+const connectDb = require('./config/db');
+
+//connecting to database
+connectDb();
+
+app.listen(8080, () => {
+    console.log('Server is running in port 8080');
+})
